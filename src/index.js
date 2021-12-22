3f7eba9b1e3bb81ee8e26dd0d9e7ded9f2b3414d
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2021-12-22 23:16:38 +0530

    Added store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+//import packages up and file below
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+const store = createStore(movies);
+console.log('store',store);
+console.log('State',store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
